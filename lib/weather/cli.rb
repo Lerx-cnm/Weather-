dcb054f8ca974331bb71f95442be46f511c8b148
Nathan Steed <steedn@outlook.com>
2020-08-24 10:29:33 +0000

    Finished the three classes, and ran tests to ensure efficiency

--- lib/weather/cli.rb
+++ lib/weather/cli.rb
@@ -1,33 +1,25 @@
 class CLI
+  def greeting
+    puts "Welcome User!"
+  end
   def start
-    puts "Welcome User1"
     puts "Please enter a zipcode to check current weather:"
     input = gets.strip
-    if input.to_i.digits.length < 5
-      puts "Please enter a valid US zipcode:"
+    puts "Verifying location..."
+    if input.to_i.digits.length != 5
+      puts "Please enter a valid US zipcode."
       start
     else
-    new_hash = API.get_weather(input)
-    @@new_hash = new_hash
-    puts "Thank you."
-    # binding.pry
+    @@new_hash = API.get_weather(input)
+    puts "Thank you for waiting."
+    response
     end
-
   end
   def conversion(input)
-    puts "Would you like units in Celsius or Farenheit?"
-    input1 = gets.strip
-    if input1 == "Celsius" || "celsius"
-      temp1 = input - 273.15
-      response(temp1)
-    elsif input1 == "Farenheit" || "farenheit"
-      temp1 = (input - 273.15)*(9/5)+32
-      response(temp1)
-    else
-      puts "Please enter a valid answer(Celsius or Farenheit)"
-    end
-
+    temp1 = (input - 273.15)*9/5 + 32
+    temp1.round
+  end
   def response
-    puts "The temperature is currently #{conversion(@@new_hash["temp"])}."
+    puts "The temperature is currently #{conversion(@@new_hash["temp"])}°F, with the minimum at #{conversion(@@new_hash["temp_min"])}°F, and the maximum at #{conversion(@@new_hash["temp_max"])}°F"
   end
 end
